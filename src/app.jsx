461b3c248b1e3a1dafc1fe6d2e9ddc5652c87840
arif-hstu <arif.dvm.hstu@gmail.com>
2021-01-25 02:10:19 +0600

    issue tracker project started

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,44 @@
-const element = (
-    <div title = 'Outer div'>
-        <h1>Hello World!</h1>
-    </div>
-);
+
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for a form to add an issue.</div>
+
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for a form to add an issue.</div>
+        )
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr/>
+                <IssueTable />
+                <hr/>
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const element = <IssueList />;
+
 ReactDOM.render(element, document.getElementById('content'));
